4c016079fbdbf50fb691b92083e90fd575a84295
Vladimir Ovechkin <ovchingus@gmail.com>
2019-10-27 12:11:12 -0700

    Update Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,18 +2,13 @@ import React from 'react'
 
 import './style.css'
 
-function Footer () {
+function Footer ({ changeLng }) {
   return (
     <footer className='footer'>
-      <div className='email'>
-        <input className='input-form' placeholder='Send me a message!' />
-      </div>
-      <div className='contacts'>
-        <ul>
-          <li><a href='tel:+79045133706'>+7(904)-513-37-06</a></li>
-          <li><a href='mailto:maxpavlov9991@gmail.com'>maxpavlov9991@gmail.com</a></li>
-          <li><a href='https://github.com/maxpavlov9991'>GitHub</a></li>
-        </ul>
+      <div className='wrapper'>
+        <a className='contacts' href='tel:+79045133706'>+7(904)-513-37-06</a>
+        <a className='contacts' href='mailto:maxpavlov9991@gmail.com'>maxpavlov9991@gmail.com</a>
+        <a className='contacts' href='https://github.com/maxpavlov9991'>GitHub</a>
       </div>
     </footer>
   )
