32a5cc1f69f790a43258d8983fd9ad701271cde2
maxpavlov9991 <maxpavlov9991@gmail.com>
2019-10-27 05:35:30 +0300

    new design

--- src/components/DumbPage/DumbPage.js
+++ src/components/DumbPage/DumbPage.js
@@ -4,55 +4,87 @@ import './style.css'
 import { useTranslation } from 'react-i18next'
 
 function DumbPage ({ changeLanguage }) {
-
   const { t } = useTranslation()
 
   return (
-    <div className='main-block'>
+    <div className='all-block'>
       <header className='header'>
         <div className='header-logo'>
-          <span className='header-logo-text'>Logo</span>
+          <a href='#' className='header-logo-text'>Logo</a>
         </div>
         <div className='header-links'>
-          <a>Link#1 </a>
-          <a>Link#2 </a>
-          <a>Link#3 </a>
+          <a href='#'>Link#1</a>
+          <a href='#'>Link#2</a>
+          <a href='#'>Link#3</a>
         </div>
         <div className='header-profile'>
-          <a>Profile</a>
+          <a href='#'>Profile</a>
         </div>
       </header>
-      <main className='main'>
-        <div className='profile-block content-block'>
-          <div className='profile-img'>
-            <div className='img-block'>
-              <img className='img' src='https://i0.wp.com/www.gottabemobile.com/wp-content/uploads/2018/09/How-to-Use-Memoji-iOS-12-iPhone-Xs-1.jpg?fit=1027%2C1027&ssl=1' alt='ProfilePhoto' />
+
+      <div className='main-content'>
+
+        <main className='main'>
+          <div className='profile-block content-block'>
+            <div className='profile-img'>
+              <div className='img-block'>
+                <img className='img' src='https://i0.wp.com/www.gottabemobile.com/wp-content/uploads/2018/09/How-to-Use-Memoji-iOS-12-iPhone-Xs-1.jpg?fit=1027%2C1027&ssl=1' alt='ProfilePhoto' />
+              </div>
+            </div>
+            <div className='profile-info'>
+              <span>{t('profile.text')}</span>
             </div>
           </div>
-          <div className='profile-info'>
-            <span>{t('profile.text')}</span>
+          <div className='something-block content-block'>
+            <button onClick={() => changeLanguage('ru')}>ru</button>
+            <button onClick={() => changeLanguage('en')}>en</button>
           </div>
-        </div>
-        <div className='something-block content-block'>
-          <button onClick={() => changeLanguage('ru')}>ru</button>
-          <button onClick={() => changeLanguage('en')}>en</button>
-        </div>
-        <div className='third-block content-block'>Third block</div>
-        <div className='fourth-block content-block'>Fourth block</div>
-        <div className='fifth-block content-block'>Fifth block</div>
-      </main>
-      <footer className='footer'>
-        <div className='footer-email'>
-          <input className='input-form' placeholder='Send me a message!' />
-        </div>
-        <div className='footer-contacts'>
-          <ul>
-            <li><a href='tel:+79045133706'>+7(904)-513-37-06</a></li>
-            <li><a href='mailto:maxpavlov9991@gmail.com'>maxpavlov9991@gmail.com</a></li>
-            <li><a href='https://github.com/maxpavlov9991'>GitHub</a></li>
-          </ul>
-        </div>
-      </footer>
+          <div className='third-block content-block'>
+            <span>Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+              et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+              ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum
+            </span>
+          </div>
+          <div className='fourth-block content-block'>
+            <span>Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum
+            </span>
+          </div>
+          <div className='fifth-block content-block'>
+            <span>Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum
+            </span>
+          </div>
+        </main>
+
+        <footer className='footer'>
+          <div className='footer-email'>
+            <input className='input-form' placeholder='Send me a message!' />
+          </div>
+          <div className='footer-contacts'>
+            <ul>
+              <li><a href='tel:+79045133706'>+7(904)-513-37-06</a></li>
+              <li><a href='mailto:maxpavlov9991@gmail.com'>maxpavlov9991@gmail.com</a></li>
+              <li><a href='https://github.com/maxpavlov9991'>GitHub</a></li>
+            </ul>
+          </div>
+        </footer>
+
+      </div>
     </div>
   )
 }
