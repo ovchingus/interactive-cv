f7fad42727ac4128dc39b259b0f6c3c106313806
Vladimir Ovechkin <ovchingus@gmail.com>
2019-10-28 11:51:15 -0700

    Add UserInfo component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import './style.css'
 
+import { UserInfo } from '../../components/UserInfo'
+
 function Main () {
   return (
     <main className='main'>
-      <div className='profile-block content-block'>
-        <div className='profile-img'>
-          <div className='img-block'>
-            <img className='img' src='https://i0.wp.com/www.gottabemobile.com/wp-content/uploads/2018/09/How-to-Use-Memoji-iOS-12-iPhone-Xs-1.jpg?fit=1027%2C1027&ssl=1' alt='ProfilePhoto' />
-          </div>
-        </div>
-        <div className='profile-info'>
-          <span>asdasd</span>
-        </div>
-      </div>
+      <UserInfo />
       <div className='something-block content-block'>
         <button>ru</button>
         <button>en</button>
---
+++ src/components/UserInfo/UserInfo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './styles.css'
+
+function UserInfo () {
+  return (
+    <div className='profile-block content-block'>
+      <div className='profile-img'>
+        <div className='img-block'>
+          <img className='img' src='https://i0.wp.com/www.gottabemobile.com/wp-content/uploads/2018/09/How-to-Use-Memoji-iOS-12-iPhone-Xs-1.jpg?fit=1027%2C1027&ssl=1' alt='ProfilePhoto' />
+        </div>
+      </div>
+      <div className='profile-info'>
+        <span>asdasd</span>
+      </div>
+    </div>
+  )
+}
+
+export default UserInfo
