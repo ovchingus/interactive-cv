1f85617e4e8c28a6418c85f9c98a2c90b118c347
Vladimir Ovechkin <ovchingus@gmail.com>
2019-11-03 21:52:26 +0300

    Add config.js file

---
+++ src/config.js
@@ -0,0 +1,5 @@
+/* If host on github pages and not as main page e.x. username.github.io
+specify exact path from root.
+To host in username.github.io/mypage option should be 'mypage' */
+
+export const hostPath = 'interactive-cv'
